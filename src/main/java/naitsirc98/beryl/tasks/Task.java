5ced1a98f6ce1fc3bb160eb048ba29e679815d2c
Cristian Herrera <naitsirc98@gmail.com>
2020-02-22 17:46:45 +0000

    Add Task javadoc

--- src/main/java/naitsirc98/beryl/tasks/Task.java
+++ src/main/java/naitsirc98/beryl/tasks/Task.java
@@ -4,18 +4,35 @@ import naitsirc98.beryl.logging.Log;
 
 import java.util.concurrent.atomic.AtomicReference;
 
+/**
+ * Base class for all asynchronous tasks. They are managed by the {@link TaskManager}
+ */
 public abstract class Task implements Runnable, Comparable<Task> {
 
     private final AtomicReference<State> state;
 
+    /**
+     * Instantiates a new Task.
+     */
     protected Task() {
         state = new AtomicReference<>(State.NONE);
     }
 
+    /**
+     * Returns the current state of this task.
+     *
+     * @return the current state
+     */
     public final State state() {
         return state.get();
     }
 
+    /**
+     * Cancels the execution of this task. It only works if the task has not entered the {@link Task#perform} method.
+     *
+     * Should be used when the task is enqueued, but before being executed.
+     *
+     */
     protected final void cancel() {
         if(state() != State.ENQUEUED) {
             Log.warning("Trying to cancel a task which is not enqueued");
@@ -24,6 +41,10 @@ public abstract class Task implements Runnable, Comparable<Task> {
         }
     }
 
+    /**
+     * Executes this task
+     *
+     * */
     @Override
     public final void run() {
         try {
@@ -43,22 +64,39 @@ public abstract class Task implements Runnable, Comparable<Task> {
         }
     }
 
+    /**
+     * Gets called just before {@link Task#perform()} method. Could be canceled at this point to not continue the execution.
+     */
     protected void onStart() {
 
     }
 
-    protected void perform() {
+    /**
+     * Performs the task
+     */
+    protected abstract void perform();
 
-    }
-
+    /**
+     * Gets called whenever the task has successfully finished execution.
+     */
     protected void onFinished() {
 
     }
 
+    /**
+     * Gets called if the execution has stopped due to an error.
+     *
+     * @param error the error
+     */
     protected void onError(Throwable error) {
 
     }
 
+    /**
+     * The priority of this task
+     *
+     * @return the priority
+     */
     public int priority() {
         return 0;
     }
@@ -68,21 +106,44 @@ public abstract class Task implements Runnable, Comparable<Task> {
         return Integer.compare(priority(), other.priority());
     }
 
+
     final void state(State state) {
         this.state.set(state);
     }
+
 
     public boolean notCanceled() {
         return state() != State.CANCELED;
     }
 
+    /**
+     * State of a task representing its lifecycle
+     */
     public enum State {
 
+        /**
+         * Indicates that this task has not been submitted yet
+         */
         NONE,
+        /**
+         * The task has been submitted to the task queue, and is waiting to be executed
+         */
         ENQUEUED,
+        /**
+         * The task has been canceled
+         */
         CANCELED,
+        /**
+         * The task is being executed right now
+         */
         RUNNING,
+        /**
+         * The task has stopped due to an unexpected error
+         */
         FAILURE,
+        /**
+         * The task has successfully finished execution
+         */
         SUCCESS
 
     }
