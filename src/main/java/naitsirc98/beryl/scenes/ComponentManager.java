29ade0f089444799caf6f942a2549710ea4d20e2
Cristian Herrera <naitsirc98@gmail.com>
2020-02-22 00:32:45 +0000

    Add ComponentManager javadoc

--- src/main/java/naitsirc98/beryl/scenes/ComponentManager.java
+++ src/main/java/naitsirc98/beryl/scenes/ComponentManager.java
@@ -1,24 +1,70 @@
 package naitsirc98.beryl.scenes;
 
+/**
+ * A Component Manager is in charge of updating Components of a certain type properly.
+ *
+ * @param <T> the type parameter
+ */
 public abstract class ComponentManager<T extends Component> {
 
-    Scene scene;
 
+    private final Scene scene;
+
+    /**
+     * Instantiates a new Component manager.
+     *
+     * @param scene the scene
+     */
+    protected ComponentManager(Scene scene) {
+        this.scene = scene;
+    }
+
+    /**
+     * Returns the scene of this Component Manager
+     *
+     * @return the scene
+     */
     public final Scene scene() {
         return scene;
     }
 
-    protected abstract void init();
-
+    /**
+     * Adds an enabled component
+     *
+     * @param component the component
+     */
     protected abstract void addEnabled(T component);
 
+    /**
+     * Adds a disabled component
+     *
+     * @param component the component
+     */
     protected abstract void addDisabled(T component);
 
+    /**
+     * Indicates that the given component is enabled.
+     *
+     * @param component the component
+     */
     protected abstract void enable(T component);
 
+    /**
+     * Indicates that the given component is disabled.
+     *
+     * @param component the component
+     */
     protected abstract void disable(T component);
 
+    /**
+     * Remove the component from this Component Manager
+     *
+     * @param component the component
+     */
     protected abstract void destroy(T component);
 
+    /**
+     * Removes all components
+     */
     protected abstract void destroy();
 }
