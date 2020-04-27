6b2c7a2242c71f9b78bbd46f17334b3347de6212
Cristian Herrera <naitsirc98@gmail.com>
2020-04-27 19:08:48 +0100

    Put Tree model and textures in resources folder

--- examples/java/naitsirc98/beryl/examples/app1/Tree.java
+++ examples/java/naitsirc98/beryl/examples/app1/Tree.java
@@ -1,5 +1,6 @@
 package naitsirc98.beryl.examples.app1;
 
+import naitsirc98.beryl.core.BerylFiles;
 import naitsirc98.beryl.core.Time;
 import naitsirc98.beryl.graphics.GraphicsFactory;
 import naitsirc98.beryl.graphics.textures.Texture;
@@ -15,7 +16,7 @@ import naitsirc98.beryl.scenes.components.behaviours.UpdateBehaviour;
 import naitsirc98.beryl.scenes.components.math.Transform;
 import naitsirc98.beryl.scenes.components.meshes.StaticMeshInstance;
 
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.Random;
 
 import static naitsirc98.beryl.util.Maths.clamp;
@@ -29,9 +30,13 @@ public class Tree {
 
         if(treeModel == null) {
 
-            treeModel = StaticModelLoader.get()
-                    .load(Paths.get("C:\\Users\\naits\\Downloads\\uploads_files_1970932_conifer_macedonian_pine(1)\\OBJ format\\conifer_macedonian_pine.obj"),
-                            false, new StaticVertexHandler.Builder().positionFunction(p -> p.mul(0.01f)).build());
+            Path modelPath = BerylFiles.getPath("models/tree.obj");
+
+            StaticVertexHandler handlerToMakeModelSmaller = new StaticVertexHandler.Builder()
+                    .positionFunction(p -> p.mul(0.012f))
+                    .build();
+
+            treeModel = StaticModelLoader.get().load(modelPath, false, handlerToMakeModelSmaller);
 
             setTreeMaterials(treeModel);
         }
@@ -62,30 +67,34 @@ public class Tree {
             float y;
 
             do {
+
                 x = random.nextInt(terrainSize);
                 z = random.nextInt(terrainSize);
                 y = terrainMesh.heightMap().heightAt(0, 0, x, z);
+
             } while(y <= minY);
 
             tree.get(Transform.class).position(x, y - 1, z);
         }
-
     }
 
     private static void setTreeMaterials(StaticModel treeModel) {
 
-        String texturesDir = "C:\\Users\\naits\\Downloads\\uploads_files_1970932_conifer_macedonian_pine(1)\\OBJ format\\";
         GraphicsFactory g = GraphicsFactory.get();
 
         PhongMaterial trunkMaterial = (PhongMaterial) treeModel.meshView("conifer_macedonian_pine_5").material();
-        trunkMaterial.colorMap(g.newTexture2D(texturesDir + "Bark_Color.png", PixelFormat.SRGBA).setQuality(Texture.Quality.MEDIUM));
+        trunkMaterial.colorMap(g.newTexture2D(getTexturePath("Bark_Color.png"), PixelFormat.SRGBA).setQuality(Texture.Quality.MEDIUM));
 
         PhongMaterial capMaterial = (PhongMaterial) treeModel.meshView("/Game/Cap_Branch_Mat_Cap_Branch_Mat").material();
-        capMaterial.colorMap(g.newTexture2D(texturesDir + "Cap_Color.png", PixelFormat.SRGBA).setQuality(Texture.Quality.LOW));
+        capMaterial.colorMap(g.newTexture2D(getTexturePath("Cap_Color.png"), PixelFormat.SRGBA).setQuality(Texture.Quality.LOW));
 
         PhongMaterial leafMaterial = (PhongMaterial) treeModel.meshView("/Game/conifer_macedonian_pine_Leaf_Mat_conifer_macedonian_pine_Leaf_Mat").material();
-        leafMaterial.colorMap(g.newTexture2D(texturesDir + "conifer_macedonian_pine_Color.png", PixelFormat.SRGBA).setQuality(Texture.Quality.HIGH));
+        leafMaterial.colorMap(g.newTexture2D(getTexturePath("Leaf_Color.png"), PixelFormat.SRGBA).setQuality(Texture.Quality.HIGH));
         leafMaterial.diffuseMap().sampler().lodBias(-1.5f);
+    }
+
+    private static String getTexturePath(String textureName) {
+        return BerylFiles.getString("textures/tree/"+textureName);
     }
 
     public static class TreeRandomBouncing extends UpdateBehaviour {
@@ -114,8 +123,6 @@ public class Tree {
         @Override
         public void onUpdate() {
             get(Transform.class).rotateZ(sin(Time.time() * bouncingSpeed) / bouncingLimit);
-            // bouncingSpeed = clamp(normalBouncingSpeed, 10.0f, bouncingSpeed - Time.IDEAL_DELTA_TIME * 0.01f);
-            // bouncingLimit = clamp(88, normalBouncingLimit, bouncingLimit + Time.IDEAL_DELTA_TIME * 0.05f);
         }
     }
 }
